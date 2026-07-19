e0267fe706e2363a67d34ccbafe962e27c94f4fb
Md.Zilhaj Un Noor <zilhajsajid@gmail.com>
2026-07-19 17:31:05 +0600

    fix: resolve local issues

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@ import app from "./app";
 import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const PORT = config.port;
-async function main() {
-  try {
-    await prisma.$connect();
-    console.log("DB connected Successfully!");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port:${PORT}`);
-    });
-  } catch (error) {
-    console.log("Error starting the server:", error);
-    await prisma.$disconnect();
-    process.exit(1);
-  }
-}
-main();
+// const PORT = config.port;
+// async function main() {
+//   try {
+//     await prisma.$connect();
+//     console.log("DB connected Successfully!");
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port:${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error starting the server:", error);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   }
+// }
+// main();
+export default app;
